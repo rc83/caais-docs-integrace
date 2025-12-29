44e85c3f3acd0965d5f840f3bae686cc6fb512d4
Richard Chudoba <richard.chudoba@dia.gov.cz>
2025-12-29 18:48:03 +0100

    Poskytuj requirements.txt pro pip

---
+++ source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CAAIS'
+copyright = '2025 Digitální a informační agentura'
+author = 'Milan Babic (NAKIT); Richard Chudoba (DIA)'
+release = '2.16'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  "sphinx_design",
+]
+
+#templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'cs'
+
+autosectionlabel_prefix_document = True
+smartquotes = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
